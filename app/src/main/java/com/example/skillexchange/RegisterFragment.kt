44db1b2f834ca1f420317729de9d8d212c3d5066
gorgontt <166929134+gorgontt@users.noreply.github.com>
2024-10-04 21:43:31 +0300

    Intent from  SignInFragment to RegisterFragment and on the contrary

--- app/src/main/java/com/example/skillexchange/RegisterFragment.kt
+++ app/src/main/java/com/example/skillexchange/RegisterFragment.kt
@@ -1,10 +1,12 @@
 package com.example.skillexchange
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import javax.annotation.Nullable
 
 
 class RegisterFragment : Fragment() {
@@ -19,7 +21,25 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
-    companion object {
+    override fun onActivityCreated(@Nullable savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        val arrowButton: View? = view?.findViewById(R.id.arrow_btn_image_registerFrag)
+        val signInBtn: View? = view?.findViewById(R.id.sign_in_tv_from_registerFrag)
+
+        arrowButton?.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
+        signInBtn?.setOnClickListener {
+            val signInrFragment = SignInFragment()
+
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.frame_register, signInrFragment)
+            transaction?.commit()
+        }
     }
+
 }
--- app/src/main/java/com/example/skillexchange/SignInFragment.kt
+++ app/src/main/java/com/example/skillexchange/SignInFragment.kt
@@ -1,15 +1,20 @@
 package com.example.skillexchange
 
+
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import javax.annotation.Nullable
 
 
 class SignInFragment : Fragment() {
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,7 +23,29 @@ class SignInFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
-    companion object {
+    override fun onActivityCreated(@Nullable savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        val arrowButton: View? = view?.findViewById(R.id.arrow_btn_image)
+        val registerTV: View? = view?.findViewById(R.id.register_tv_from_signIn)
+
+        arrowButton?.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
+        registerTV?.setOnClickListener {
+
+            val registerFragment = RegisterFragment()
+
+
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.frame_sign_in, registerFragment)
+//            transaction?.addToBackStack(null)
+            transaction?.commit()
+        }
     }
+
+
 }
